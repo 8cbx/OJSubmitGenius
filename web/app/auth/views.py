a8092cfec63b80138d29c4971061253b2dd98806
Marylandeny-Chu <chu849238686@foxmail.com>
2015-06-09 18:45:05 +0800

    Change auth

--- web/app/auth/views.py
+++ web/app/auth/views.py
@@ -6,8 +6,9 @@ from .. import db
 from ..models import User
 from ..email import send_email
 from .forms import LoginForm, RegistrationForm, ChangePasswordForm,\
-    PasswordResetRequestForm, PasswordResetForm, ChangeEmailForm
-
+    PasswordResetRequestForm, PasswordResetForm, ChangeEmailForm,\
+    EditProfileForm, EditProfileAdminForm
+from ..decorators import admin_required
 
 @auth.before_app_request
 def before_request():
@@ -25,6 +26,58 @@ def unconfirmed():
         return redirect(url_for('main.index'))
     return render_template('auth/unconfirmed.html')
 
+@auth.route('/user/<username>')
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    return render_template('auth/user.html', user=user)
+
+@auth.route('/edit-profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.name = form.name.data
+        current_user.country = form.country.data
+        current_user.school = form.school.data
+        current_user.about_me = form.about_me.data
+        db.session.add(current_user)
+        flash('Your profile has been updated.')
+        return redirect(url_for('.user', username=current_user.username))
+    form.name.data = current_user.name
+    form.country.data = current_user.country
+    form.school.data = current_user.school
+    form.about_me.data = current_user.about_me
+    return render_template('auth/edit_profile.html', form=form)
+
+
+
+@auth.route('/edit-profile/<int:id>', methods=['GET', 'POST'])
+@login_required
+@admin_required
+def edit_profile_admin(id):
+    user = User.query.get_or_404(id)
+    form = EditProfileAdminForm(user=user)
+    if form.validate_on_submit():
+        user.email = form.email.data
+        user.username = form.username.data
+        user.confirmed = form.confirmed.data
+        user.role = Role.query.get(form.role.data)
+        user.name = form.name.data
+        user.country = form.country.data
+        user.school = form.school.data
+        user.about_me = form.about_me.data
+        db.session.add(user)
+        flash('The profile has been updated.')
+        return redirect(url_for('.user', username=user.username))
+    form.email.data = user.email
+    form.username.data = user.username
+    form.confirmed.data = user.confirmed
+    form.role.data = user.role_id
+    form.name.data = user.name
+    form.country.data = user.country
+    form.school.data = user.school
+    form.about_me.data = user.about_me
+    return render_template('auth/edit_profile.html', form=form, user=user)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
