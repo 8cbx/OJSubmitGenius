8c7c9a4330e4f3d593d45631cf06dae8c8821077
burglar <burglar@aliyun.com>
2015-06-17 20:49:23 +0800

    create paginate

--- web/app/main/views.py
+++ web/app/main/views.py
@@ -1,16 +1,21 @@
-from flask import render_template, redirect, url_for, abort, flash
+from flask import render_template, redirect, url_for, abort, flash, request,\
+    current_app
 from flask.ext.login import login_required, current_user
 from . import main
 from .forms import EditProfileForm, EditProfileAdminForm
 from .. import db
-from ..models import Role, User, Problem
+from ..models import Permission, Role, User, Problem
 from ..decorators import admin_required
 
 
 @main.route('/problem', methods=['GET', 'POST'])
-def indexProblrm():
-	problems = Problem.query.order_by(Problem.LastUpdate.desc()).all()
-	return render_template('indexProblem.html', problems = problems)
+def indexProblem():
+	page = request.args.get('page', 1, type=int)
+	pagination = Problem.query.order_by(Problem.LastUpdate.desc()).paginate(
+       page, per_page=current_app.config['FLASKY_POSTS_PER_PAGE'],
+		error_out=False)
+	problems = pagination.items
+	return render_template('indexProblem.html',problems=problems, pagination=pagination)
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
